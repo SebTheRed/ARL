072e0059c782bc63f88b5dc64554654f32915453
SebThRed <91200399+SebTheRed@users.noreply.github.com>
2023-08-12 11:22:02 -0400

    Trying / Failing to start a react native project on IOS

---
+++ arl/arl/arlApp.swift
@@ -0,0 +1,17 @@
+//
+//  arlApp.swift
+//  arl
+//
+//  Created by Sebastian Belfonti on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct arlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8f9de8bf8a0b35dbcf29fc7d2dfb240f28f78cf2
SebThRed <91200399+SebTheRed@users.noreply.github.com>
2023-08-25 18:10:46 -0400

    fixed tsConfig.json

--- mobile/index.js
+++ mobile/index.js
@@ -2,6 +2,8 @@
  * @format
  */
  import 'react-native-gesture-handler';
+ import { LogBox } from 'react-native';
+ LogBox.ignoreLogs(['Sending `onAnimatedValueUpdate` with no listeners registered.']);
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';

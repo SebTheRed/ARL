03aedb51a37d1bdb6153ba49d6ee61d8a0866cd6
SebThRed <91200399+SebTheRed@users.noreply.github.com>
2023-08-25 20:53:40 -0400

    user geo-location now works

--- mobile/Pages/Map/Map.tsx
+++ mobile/Pages/Map/Map.tsx
@@ -7,19 +7,35 @@ import {
     useColorScheme,
     View,
   } from 'react-native';
-import React from 'react'
+import React,{useMemo,useState} from 'react'
 import styles from '../../styles'
 import MapView from 'react-native-maps';
+import Config from "react-native-config";
+import Geolocation from '@react-native-community/geolocation';
+
 
 ////// COMPONENT FUNCTION BEGINNING //////
 const Map = ():JSX.Element=>{
+// Use Config.GOOGLE_MAPS_API_KEY
+const apiKey = Config.GOOGLE_MAPS_API_KEY;
+const [userGeoData,setUserGeoData] = useState({
+  latitude:40.8259,longitude:-74.2090,
+})
+useMemo(()=>{
+  Geolocation.getCurrentPosition(info => {
+    console.log(info.coords.latitude, info.coords.longitude);
+    setUserGeoData({latitude:info.coords.latitude, longitude:info.coords.longitude})
+  });
+},[])
+
+
     return(
         <View style={{flex:1}}>
             <MapView
             style={{width:"100%",height:"100%"}}
             initialRegion={{
-                latitude: 40.8259,
-                longitude: -74.2090,
+                latitude: userGeoData.latitude,
+                longitude: userGeoData.longitude,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
               }}

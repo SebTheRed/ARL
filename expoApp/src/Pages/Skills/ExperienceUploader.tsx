49090183c5bc6a24d617a856eca6a2b81b12da73
SebThRed <91200399+SebTheRed@users.noreply.github.com>
2023-09-20 20:02:16 -0400

    re-nested logic to wait for cameraPicURL, now posts have the link to the pic :)

--- expoApp/src/Pages/Skills/ExperienceUploader.tsx
+++ expoApp/src/Pages/Skills/ExperienceUploader.tsx
@@ -116,62 +116,63 @@ const ExperienceUploader = ():JSX.Element => {
             },
             async () => {
                 pictureURL = await getDownloadURL(imageRef)
+                const postObj = {
+                    cameraPicURL:pictureURL,
+                    posterUID:uid,
+                    posterUserName:userData.userName,
+                    streak:userData.streak,
+                    postSkill:currentEvent.skillTitle,
+                    picURL: userData.picURL,
+                    uniqueStamp:"",
+                    eventTitle:currentEvent.title,
+                    xp:currentEvent.xp,
+                    score:0,
+                    geoTag:{latitude:0,longitude:0},
+                    timeStamp:timeStamp,
+                    textLog:text,
+                    publicPost:settingOne,
+                    mapPost:settingTwo,
+                    globalPost:settingThree,
+                    id: postID,
+                    uid: uid,
+                    type:currentEvent.type
+                }
+                // if (settingOne == true) {postObj.geoTag = await getGeoLocation() as { latitude: number; longitude: number };}
+                try {
+                    const uniqueUserPath = `users/${uid}/xpLog`
+                    await setDoc(doc(db,uniqueUserPath, postID), {id: postID, timeStamp:timeStamp, eventTitle:currentEvent.title, traitType:currentEvent.skillTitle, xp:currentEvent.xp })
+                    await setDoc(doc(db, "posts", postID),postObj)
+                    .then(() => {
+                        console.log("Post Success!");
+                        // Go back to the root navigator
+                        newPostHandler()
+                        navigation.dispatch(
+                            CommonActions.reset({
+                              index: 0,
+                              routes: [
+                                {
+                                  name: 'AuthedApp', // The name of the root navigator's screen that contains the child navigators
+                                  state: {
+                                    routes: [
+                                      {
+                                        name: 'Feed', // The name of the child navigator
+                                      },
+                                    ],
+                                  },
+                                },
+                              ],
+                            })
+                          );
+                        })
+                } catch(err){
+                    console.error("Post failed to post",err)
+                }
             }
             )
             } catch(err) {
                 console.error("upload failed",err)
             }
-            const postObj = {
-                cameraPicURL:pictureURL,
-                posterUID:uid,
-                posterUserName:userData.userName,
-                streak:userData.streak,
-                postSkill:currentEvent.skillTitle,
-                picURL: userData.picURL,
-                uniqueStamp:"",
-                eventTitle:currentEvent.title,
-                xp:currentEvent.xp,
-                score:0,
-                geoTag:{latitude:0,longitude:0},
-                timeStamp:timeStamp,
-                textLog:text,
-                publicPost:settingOne,
-                mapPost:settingTwo,
-                globalPost:settingThree,
-                id: postID,
-                uid: uid,
-                type:currentEvent.type
-            }
-            // if (settingOne == true) {postObj.geoTag = await getGeoLocation() as { latitude: number; longitude: number };}
-            try {
-                const uniqueUserPath = `users/${uid}/xpLog`
-                await setDoc(doc(db,uniqueUserPath, postID), {id: postID, timeStamp:timeStamp, eventTitle:currentEvent.title, traitType:currentEvent.skillTitle, xp:currentEvent.xp })
-                await setDoc(doc(db, "posts", postID),postObj)
-                .then(() => {
-                    console.log("Post Success!");
-                    // Go back to the root navigator
-                    newPostHandler()
-                    navigation.dispatch(
-                        CommonActions.reset({
-                          index: 0,
-                          routes: [
-                            {
-                              name: 'AuthedApp', // The name of the root navigator's screen that contains the child navigators
-                              state: {
-                                routes: [
-                                  {
-                                    name: 'Feed', // The name of the child navigator
-                                  },
-                                ],
-                              },
-                            },
-                          ],
-                        })
-                      );
-                    })
-            } catch(err){
-                console.error("Post failed to post",err)
-            }
+
         } else {
             console.warn("LOG MUST BE AT LEAST 16 CHARACTERS")
         }

5cf785251bbd2cf84d3dc02790eb2001fb50bcab
SebThRed <91200399+SebTheRed@users.noreply.github.com>
2023-09-23 16:59:10 -0400

    she finally works :,) Can't trust GPT with such complex things

--- expoApp/src/Contexts/CooldownContext.tsx
+++ expoApp/src/Contexts/CooldownContext.tsx
@@ -17,50 +17,74 @@ export const CooldownProvider = ({ children }: any) => {
     const [cooldownsLoading, setCooldownsLoading] = useState<boolean>(true);
 
     useEffect(() => {
+        
         const fetchCooldowns = async () => {
             const allPromises: any = [];
-    
-            for (let category in experiencesList) {
-                for (let experienceTitle in experiencesList[category]) {
-                    const q = query(
-                        collection(db, `users/${uid}/xpLog`),
-                        where('eventTitle', '==', experiencesList[category][experienceTitle].title),
-                        orderBy('timestamp', 'desc'),
-                        limit(1)
-                    );
-    
-                    allPromises.push(getDocs(q));
-                }
+            let querySnapshots:any
+            const titleToKeyMapping: any = {};
+            try {
+                Object.keys(experiencesList).forEach((skill: any, i: number) => {
+                    Object.keys(experiencesList[skill]).forEach((titleKey: any, i: number) => {
+                      const experienceTitle = experiencesList[skill][titleKey].title;
+                      titleToKeyMapping[experienceTitle] = titleKey;
+                        const q = query(
+                            collection(db,`users/${uid}/xpLog`),
+                            where('eventTitle',"==",experiencesList[skill][titleKey].title),
+                            orderBy('timeStamp','desc'),
+                            limit(1)
+                        )
+                        allPromises.push(getDocs(q))
+                    })
+                })
+            querySnapshots = await Promise.all(allPromises);
+            } catch (err) {
+                console.warn("Queries failed somehow: ",err)
             }
-    
-            const querySnapshots = await Promise.all(allPromises);
+            
+            // console.log(allPromises)
+            
             const calculatedCooldowns: any = {};
-    
-            querySnapshots.forEach((querySnapshot) => {
-                if (!querySnapshot.empty) {
-                    const docData = querySnapshot.docs[0].data();
-                    const type = docData.traitType;
-
-                    const experienceCooldown = experiencesList[type][docData.eventTitle].cooldown * 60 * 60 * 1000;
-                    const [year, month, day, hour, minute, second] = docData.timestamp.split("-").map(Number);
-                    const lastPostedDate = new Date(year, month - 1, day, hour, minute, second);
-                    const lastPostedTime = lastPostedDate.getTime();
-                    const currentTime = new Date().getTime();
-                    const elapsedTime = currentTime - lastPostedTime;
-                    const remainingCooldown = experienceCooldown - elapsedTime;
-
-                    if (remainingCooldown > 0) {
-                        calculatedCooldowns[docData.eventTitle] = remainingCooldown / (60 * 60 * 1000);
+            // console.log("Query Snapshots:", querySnapshots);
+            console.log("Query snapshots length: ",querySnapshots.length)
+            try{
+                querySnapshots.forEach((querySnapshot:any) => {
+                    if (!querySnapshot.empty) {
+                        const docData = querySnapshot.docs[0].data();
+                        console.log(docData)
+                        const type = (docData.traitType).toLowerCase();
+                        const experienceKey = titleToKeyMapping[docData.eventTitle];
+                        console.log("exp key: ",experienceKey)
+                        if (experienceKey) {
+                                console.log("inside exp key")
+                                console.log(experiencesList[type][experienceKey].cooldown)
+                                const experienceCooldown = experiencesList[type][experienceKey].cooldown * 60 * 60 * 1000;
+                                const [year, month, day, hour, minute, second] = docData.timeStamp.split("-").map(Number);
+                                const lastPostedDate = new Date(year, month - 1, day, hour, minute, second);
+                                const lastPostedTime = lastPostedDate.getTime();
+                                const currentTime = new Date().getTime();
+                                const elapsedTime = currentTime - lastPostedTime;
+                                const remainingCooldown = experienceCooldown - elapsedTime;
+            
+                                if (remainingCooldown > 0) {
+                                    calculatedCooldowns[docData.eventTitle] = remainingCooldown / (60 * 60 * 1000);
+                                }
+                        } else {
+                            console.warn("no experienceKey match",docData.eventTitle)
+                        }
+                        
                     }
-                }
-            });
+                });
+            } catch(err) {
+                console.warn("document parsing failed: ", err)
+            }
+            
     
             setCooldowns(calculatedCooldowns);
             setCooldownsLoading(false);
             console.log("calced cooldowns: ",calculatedCooldowns)
         };
     
-        if (!dataLoading) {
+        if (dataLoading==false) {
             fetchCooldowns();
         }
     }, [dataLoading]);

fa02bc2ff0b9f0dd0af8a4e3887989d6005fe8dc
SebThRed <91200399+SebTheRed@users.noreply.github.com>
2023-09-23 14:21:49 -0400

    i give up for a while

--- expoApp/src/Contexts/CooldownContext.tsx
+++ expoApp/src/Contexts/CooldownContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { collection,where,orderBy,limit,query,getDocs } from 'firebase/firestore';
+import { collection, where, orderBy, limit, query, getDocs } from 'firebase/firestore';
 import { db } from '../Firebase/firebase';
 import { useGameRules } from './GameRules';
 import { useUID } from './UIDContext';
@@ -11,70 +11,65 @@ export const useCooldowns = () => {
 }
 
 export const CooldownProvider = ({ children }: any) => {
-    const {uid}:any = useUID();
-    const {experiencesList}:any = useGameRules();
-    const [cooldowns, setCooldowns] = useState<any>();
+    const { uid }: any = useUID();
+    const { experiencesList, dataLoading }: any = useGameRules();
+    const [cooldowns, setCooldowns] = useState<any>({});
     const [cooldownsLoading, setCooldownsLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const fetchCooldowns = async () => {
-            const allPromises:any = [];
-            // For each category, fetch the most recent log for each experience
-            Object.keys(experiencesList).forEach(category => {
-                console.log("Category", category);
-                
-                Object.keys(experiencesList[category]).forEach(experienceTitle => {
-                    console.log("ExperienceTitle", experienceTitle);
-                    
+            const allPromises: any = [];
+    
+            for (let category in experiencesList) {
+                for (let experienceTitle in experiencesList[category]) {
                     const q = query(
-                        collection(db, `${uid}/xpLog`),
+                        collection(db, `users/${uid}/xpLog`),
                         where('eventTitle', '==', experiencesList[category][experienceTitle].title),
                         orderBy('timestamp', 'desc'),
                         limit(1)
                     );
-                    
-                    const promise = getDocs(q).then((querySnapshot: any) => {
-                        if (!querySnapshot.empty) {
-                            return {
-                                eventTitle: experienceTitle,
-                                data: querySnapshot.docs[0].data()
-                            };
-                        }
-                        return null;
-                    });
-                    allPromises.push(promise);
-                });
-            });
-            
-            // Once all queries are done
-            const results = await Promise.all(allPromises);
-            const calculatedCooldowns = {};
+    
+                    allPromises.push(getDocs(q));
+                }
+            }
+    
+            const querySnapshots = await Promise.all(allPromises);
+            const calculatedCooldowns: any = {};
+    
+            querySnapshots.forEach((querySnapshot) => {
+                if (!querySnapshot.empty) {
+                    const docData = querySnapshot.docs[0].data();
+                    const type = docData.traitType;
 
-            results.forEach(result => {
-                if (result) {
-                    const category = result.eventTitle;
-                    const experienceCooldown = experiencesList[category][result.eventTitle].cooldown;
-                    const lastPostedTime = result.data.timestamp;
+                    const experienceCooldown = experiencesList[type][docData.eventTitle].cooldown * 60 * 60 * 1000;
+                    const [year, month, day, hour, minute, second] = docData.timestamp.split("-").map(Number);
+                    const lastPostedDate = new Date(year, month - 1, day, hour, minute, second);
+                    const lastPostedTime = lastPostedDate.getTime();
                     const currentTime = new Date().getTime();
                     const elapsedTime = currentTime - lastPostedTime;
                     const remainingCooldown = experienceCooldown - elapsedTime;
 
                     if (remainingCooldown > 0) {
-                        calculatedCooldowns[result.eventTitle] = remainingCooldown;
+                        calculatedCooldowns[docData.eventTitle] = remainingCooldown / (60 * 60 * 1000);
                     }
                 }
             });
-
+    
             setCooldowns(calculatedCooldowns);
             setCooldownsLoading(false);
+            console.log("calced cooldowns: ",calculatedCooldowns)
         };
-
-        fetchCooldowns();
-    }, []);
+    
+        if (!dataLoading) {
+            fetchCooldowns();
+        }
+    }, [dataLoading]);
 
     return (
-        <CooldownContext.Provider value={{cooldowns, cooldownsLoading}}>
+        <CooldownContext.Provider value={{ cooldowns, cooldownsLoading }}>
             {children}
         </CooldownContext.Provider>
     );
 }
+
+export default CooldownProvider;
